c254dd32733cff8322d659cf1034df941c90385a
watabr <watabr.tech@gmail.com>
2022-02-17 08:50:18 -0300

    Finish implementing pending design patterns

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # dev-concepts
 My examples about dev basics concepts
-
-## TODO
-- Implements concepts/design-patterns/gang-of-four/:
-    - decorator
-    - prototype
--- concepts/design-patterns/gang-of-four/prototype/prototype.py
+++ concepts/design-patterns/gang-of-four/prototype/prototype.py
@@ -1,1 +1,56 @@
-# Pensando em algum exemplo de negócio que faça sentido com o Prototype
+
+
+from abc import ABC
+from math import prod
+
+
+class Prototype(ABC):
+    def clone(self) -> None:
+        pass
+
+class Message(Prototype):
+    def __init__(self, action: str, product: str, price: float) -> None:
+        self._action = action
+        self._product = product
+        self._price = price
+
+    @property
+    def action(self) -> str:
+        return self._action
+
+    @action.setter
+    def action(self, action: str) -> None:
+        self._action = action
+
+    @property
+    def product(self) -> str:
+        return self._product
+
+    @product.setter
+    def product(self, product: str) -> None:
+        self._product = product
+
+    @property
+    def price(self) -> float:
+        return self._price
+
+    @price.setter
+    def price(self, price: float) -> None:
+        self._price = price
+
+    def clone(self) -> None:
+        new = self.__class__(self.action, self.product, self.price)
+        return new
+
+
+if __name__ == '__main__':
+    message = Message('create', 'instance', 10.)
+    message_cloned = message.clone()
+
+    message.action = 'update'
+
+    message_cloned.price = 11.
+
+    print(f'First message: {vars(message)}')
+    print(f'Second message: {vars(message_cloned)}')
+
